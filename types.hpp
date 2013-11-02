ae56bf6e839a08aa6e49f28b6128451a87cadffa
Warren Gay <ve3wwg@gmail.com>
2013-11-01 21:29:29 -0400

    Moved csrid_t to types.hpp

--- types.hpp
+++ types.hpp
@@ -11,7 +11,8 @@ typedef long 			regid_t;	// Registry ID type
 typedef size_t			lineno_t;	// Line no.
 typedef unsigned		colno_t;	// Column no. within a line
 
-typedef int			viewid_t;	// View
+typedef int			viewid_t;	// View id value
+typedef long			csrid_t;	// Cursor id value
 
 #endif // TYPES_HPP
 
