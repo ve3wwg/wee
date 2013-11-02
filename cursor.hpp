fee3503c82890486593a94846d9978bd54019636
Warren Gay <ve3wwg@gmail.com>
2013-11-01 21:28:32 -0400

    Moved out of buffer to it's own module

---
+++ cursor.hpp
@@ -0,0 +1,51 @@
+//////////////////////////////////////////////////////////////////////
+// cursor.hpp -- Cursor Class
+// Date: Fri Nov  1 21:20:56 2013   (C) datablocks.net
+///////////////////////////////////////////////////////////////////////
+
+#ifndef CURSOR_HPP
+#define CURSOR_HPP
+
+#include "types.hpp"
+
+#include <unordered_map>
+
+class Buffer;
+
+class Cursor {
+	csrid_t		csrid;		// ID of this cursor
+	regid_t		bufid;		// Buffer ID
+	lineno_t	lno;		// Line no.
+	colno_t		col;		// Column no. within the line
+	bool		end_file;	// True if this cursor follows end of buffer
+
+	typedef std::unordered_map<regid_t,Cursor*> csrmap_t;		// Map of cursors
+
+	static csrmap_t				   cursors_map;		// csrid => Cursor *
+	static csrid_t				   next_id;
+	static std::unordered_map<regid_t,csrmap_t> buffers_map;	// Bufid => cursors map
+
+protected:
+	void register_cursor();
+	void unregister_cursor();
+
+public:	Cursor();
+	Cursor(const char *bufname,lineno_t lno,colno_t col);
+	~Cursor();
+	
+	inline csrid_t id()		{ return csrid; }
+	inline Buffer *buffer();
+	inline lineno_t line()		{ return lno; }
+	inline colno_t column()		{ return col; }
+
+	void reassociate(Buffer *buf);
+
+	// Static methods
+	static Cursor *lookup(csrid_t id);
+	static void destroyed(regid_t bufid);
+	static void reloaded(regid_t bufid);
+};
+
+#endif // CURSOR_HPP
+
+// End cursor.hpp
